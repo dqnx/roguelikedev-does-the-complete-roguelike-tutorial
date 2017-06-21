412f0f0d484124b4af24e3dc7aaa9a94a8bcc5ce
Sean Brock <sbrock065@gmail.com>
2017-06-21 11:42:14 -0400

    Update roguelike.go

--- roguelike.go
+++ roguelike.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"gitlab.com/rauko1753/gorl"
 )
 
 func main() {
 	fmt.Println("/r/roguelikedev Tutorial!")
+	
+	// Setup terminal view
+	gorl.TermMustInit()
+	defer gorl.TermDone()
+	
 }
