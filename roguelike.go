684d146c84acf65ffcd8e884980ef2f8fbb90ef0
Sean Brock <sbrock065@gmail.com>
2017-06-22 10:21:25 -0400

    Update roguelike.go

--- roguelike.go
+++ roguelike.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/
 	"gitlab.com/rauko1753/gorl"
+	"github.com/faiface/mainthread"
+	blt "bitbucket.org/cfyzium/bearlibterminal"
 )
 
-func main() {
+func run() {
 	fmt.Println("/r/roguelikedev Tutorial!")
-	
-	// Setup terminal view
-	gorl.TermMustInit()
-	defer gorl.TermDone()
-	
+}
+
+func main() {
+	// Allows rendering to be fixed to the first os thread. Needed for Windows
+	mainthread.Run(run)
 }
