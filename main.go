cd4dd33aabfa7464a05cefa13da5746ce81e8f29
Sean Brock <sbrock065@gmail.com>
2017-06-30 14:48:26 -0400

    Comments and cleanup

--- main.go
+++ main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	blt "github.com/dqnx/bearlibterminal"
-	v2 "github.com/dqnx/roguelikedev-does-the-complete-roguelike-tutorial/vector2"
-	"github.com/faiface/mainthread"
 	"time"
+	
+	blt "github.com/dqnx/bearlibterminal"
+	v2 "github.com/dqnx/vector2"
+	"github.com/faiface/mainthread"
 )
 
 func run() {
-	// Setup terminal
+	// Setup terminal.
 	size := v2.Vector{x: 80, y: 25}
 	config := "window: size=" + size.x + "x" + size.y + ", cellsize=auto, title='roguelike'; font: default;"
 	blt.Set(config)
 	blt.Color("white")
 	blt.Bkcolor("black")
 	
+	// Open terminal.
 	blt.Open()
 	defer blt.Close()
-	
-	// Initial Screen
 	blt.Print(1, 1, "/r/roguelikedev Tutorial!")
 	
 	const (
+		// Time between draws, in nanoseconds.
 		frametime := time.Nanosecond * (1.0e9 / 60.0)
 	)
 	
 	GameLoop:
 		for {
-			// Start loop execution timer
+			// Start loop execution timer.
 			start := time.Now()
 
-			// Render buffer
+			// Render the terminal buffer.
 			blt.Refresh()
 
-			// Handle input
-
+			// Handle input.
 			for blt.HasInput() {
 				key := blt.Read()
 				switch key {
@@ -43,22 +44,28 @@ func run() {
 				}
 			}
 
-			// Update
+			// Update game logic.
 
-			// Wait for loop cycle to equal frametime duration
+			
 			finish := time.Since(start)
 			remainder := frametime.Sub(finish)
-
-			if (remainder > 0) {
-				time.Sleep(remainder)	
-			} 
-			else {
+			
+			/*	
+			If remainder is greater than zero, the loop is running behind the
+			desired frame time. This will cause frames per second to not meet the
+			target.
+			*/
+			if (remainder < 0) {
 				fmt.Println("W: Update loop is lagging behind by", remainder)
+				return
 			}
+			
+			// Wait for loop cycle to equal frametime duration.
+			time.Sleep(remainder)	
 		}
 }
 
 func main() {
-	// Enables use of graphics calls on main os thread and goroutines together
+	// Enables use of graphics calls on main os thread and goroutines together.
 	mainthread.Run(run)
 }
