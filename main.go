077b34a1b9623f956962ee2563ca7928986cc103
Sean Brock <sbrock065@gmail.com>
2017-06-30 12:40:53 -0400

    Added fps locking

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	blt "github.com/dqnx/bearlibterminal"
 	v2 "github.com/dqnx/roguelikedev-does-the-complete-roguelike-tutorial/vector2"
 	"github.com/faiface/mainthread"
+	"time"
 )
 
 func run() {
 	// Setup terminal
 	size := v2.Vector{x: 80, y: 25}
-	config := "window: size=" + size.x + "x" + size.y + ", cellsize=auto, title='Omni: menu'; font: default;"
+	config := "window: size=" + size.x + "x" + size.y + ", cellsize=auto, title='roguelike'; font: default;"
 	blt.Set(config)
 	blt.Color("white")
 	blt.Bkcolor("black")
@@ -18,16 +19,43 @@ func run() {
 	blt.Open()
 	defer blt.Close();
 	
+	// Initial Screen
 	blt.Print(1, 1, "/r/roguelikedev Tutorial!")
-	blt.Refresh()
 	
-	for running := true; running == true {
-		// Handle input
-		key := blt.Read()
-		switch key {
-			case blt.TK_CLOSE: running = false
+	const (
+		frametime := time.Nanosecond * (1.0e9 / 60.0)
+	)
+	
+	GameLoop:
+		for {
+			// Start loop execution timer
+			start := time.Now()
+
+			// Render buffer
+			blt.Refresh()
+
+			// Handle input
+
+			for blt.HasInput() {
+				key := blt.Read()
+				switch key {
+					case blt.TK_CLOSE: break GameLoop
+				}
+			}
+
+			// Update
+
+			// Wait for loop cycle to equal frametime duration
+			finish := time.Since(start)
+			remainder := frametime.Sub(finish)
+
+			if (remainder > 0) {
+				time.Sleep(remainder)	
+			} 
+			else {
+				fmt.Println("W: Update loop is lagging behind by", remainder)
+			}
 		}
-	}
 }
 
 func main() {
