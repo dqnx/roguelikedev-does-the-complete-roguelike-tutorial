ca1b6a022180aa4d058c0edb9c9d1825af2f6573
Sean Brock <sbrock065@gmail.com>
2017-07-05 13:32:40 -0400

    Added concrete struct

--- entities.go
+++ entities.go
@@ -5,27 +5,36 @@ import (
 )
 
 // Drawable defines entities which can be drawn to the console.
-type drawable interface {
+type Drawable interface {
 	draw() int
 }
 
 // Movable defines entities which have a changeable world position.
-type movable interface {
-	move(dx, dy int)
+type Movable interface {
+	// Move will add a displacement vector to a position.
+	move(delta v2.Vector)
+	
+	// Location returns the location of the movable in the game world.
+	location() v2.Vector
+}
+
+// Concrete is a physical object in the game world, with position and graphics.
+type Concrete struct {
+	Position v2.Vector
+	Code int
+	Color string
 }
 
 // Actor holds values that describe a character, player, enemy, etc.
-type actor struct {
+type Actor struct {
 	Position v2.Vector
-	Name     string
-	Code     int
+	Concrete
 }
 
-func (a actor) draw() int {
-	return a.Code
+func (a Actor) draw() string, int {
+	return a.Color, a.Code
 }
 
-func (a *actor) move(dx, dy int) {
-	a.Position.X += dx
-	a.Position.Y += dy
+func (a *Actor) move(delta v2.Vector) {
+	a.Position = v2.Add(delta, a.Position)
 }
